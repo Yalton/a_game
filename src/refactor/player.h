20fd63a5eafe5a99def94e35faad662eb7ca10e9
DaltonBailey <drbailey117@gmail.com>
2023-03-31 22:24:30 -0500

    player updated

--- src/refactor/player.h
+++ src/refactor/player.h
@@ -1,13 +1,11 @@
 #ifndef PLAYER
 #define PLAYER
-#include <iostream>
-#include <vector>
-#include "item.h"
 #include "header.h"
-using namespace std;
-class Player
-{
+
+class Player {
 private:
+    using namespace std;
+
     float hp;
     int dist;
     int delta;
@@ -16,51 +14,53 @@ private:
     int path;
     int secr;
     float dfc;
-    class ItemNode
-	{
-	public:
-		ItemNode(Item item, Item *next)
-		{
-			m_item = item;
-			m_next = next;
-		}
-		Item m_item;
-		Item *m_next;
-	};
-	Item *m_head;
+
+    struct ItemNode {
+        char type = 'n';
+        int val = 0;
+    };
+
+    struct Item {
+        ItemNode item;
+        Item* next;
+
+        Item(const ItemNode& item, Item* next)
+            : item(item), next(next) {}
+        ~Item() { delete next; }
+    };
+
+    Item* m_head;
 
 public:
-    vector<Item *> items;
     // List functions
     Player();
     ~Player();
+    bool empty() const;
+    bool remove(char c, int value);
+    int size() const;
+    string itemname(char c, int val) const;
+    int typepresent(char c) const;
+    int givenum(char c, int x) const;
+    bool doeshave(char c, int val) const;
+    void insert(char type, int val);
 
-	bool empty();
-	bool remove(char c, int value);
-	int size();
-	string itemname(char c, int val);
-	int typepresent(char c);
-	int givenum(char c, int x);
-	bool doeshave(char c, int val);
-	void insert(char type, int val);
-
-	//Setters and Getters
-	void damage(int x);
-	void setpath(int x); 
-	int getpath(); 
-	void setdelta(int x);
-	int getdelta();
-	void setdfc(float x);
-	float getdfc();
-	void setsecr(int x);
-	int getsecr();
-	void sethp(float x);
-	float gethp();
-	void setdmg(int x);
-	int getdmg();
-	void setdist(int y);
-	int getdist();
-	void setbatt(int z);
-	int getbatt();
+    // Setters and Getters
+    void damage(int x);
+    void setpath(int x);
+    int getpath() const;
+    void setdelta(int x);
+    int getdelta() const;
+    void setdfc(float x);
+    float getdfc() const;
+    void setsecr(int x);
+    int getsecr() const;
+    void sethp(float x);
+    float gethp() const;
+    void setdmg(int x);
+    int getdmg() const;
+    void setdist(int y);
+    int getdist() const;
+    void setbatt(int z);
+    int getbatt() const;
 };
 #endif
